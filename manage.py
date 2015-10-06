f89f442ca430272231d23e9d08b5a184d52224f4
rakjin <grampush@gmail.com>
2015-10-06 17:56:35 +0900

    add static files and jinja Environment
     - move sella/jinja2/*.jinja2 to jinja2/
     - add base_layout.jinja2

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from importd import d
+from jinja2 import Environment
 
 
 d(
@@ -16,12 +17,26 @@ d(
     TEMPLATES=[
         {
             'BACKEND': 'django.template.backends.jinja2.Jinja2',
+            'DIRS': ['jinja2'],
             'APP_DIRS': True,
+            'OPTIONS': {
+                'environment': 'manage.jinja2_environment',
+            },
         },
     ],
     # django-nose
     TEST_RUNNER='django_nose.NoseTestSuiteRunner',
 )
+
+from django.contrib.staticfiles.storage import staticfiles_storage
+
+
+def jinja2_environment(**options):
+    env = Environment(**options)
+    env.globals.update({
+        'static': staticfiles_storage.url,
+    })
+    return env
 
 if __name__ == '__main__':
     d.main()
