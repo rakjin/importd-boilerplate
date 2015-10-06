37e2c1e538b35b69265c2efb08074daae23a74e3
rakjin <grampush@gmail.com>
2015-10-06 18:28:08 +0900

    install django-debug-toolbar

--- manage.py
+++ manage.py
@@ -8,13 +8,27 @@ d(
     DEBUG=True,
     INSTALLED_APPS=[
         'django.contrib.auth',
+        'django.contrib.staticfiles',
 
         # external library
         'django_nose',
+        'debug_toolbar',
 
         'sella',
     ],
     TEMPLATES=[
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
         {
             'BACKEND': 'django.template.backends.jinja2.Jinja2',
             'DIRS': ['jinja2'],
